ee84247a98174741648d99ad19864abc5370b461
Sebastian-git <sebastian01cevallos@gmail.com>
2020-08-10 14:33:51 -0700

    Added NASA API functionality

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // Import and initialize all required libraries
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const app = express();
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // Set ejs as default view engine and set /public as the default directory
 app.set("view engine", "ejs");
@@ -14,9 +15,31 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", urlencodedParser, function(req, res) {
-  console.log(req.body);
-  res.render("index");
-})
+  console.log(req.body.min + " and " + req.body.max);
+  let maxDate = req.body.max;
+  let minDate = req.body.min;
+  const config = {
+    method: "get",
+    url: `https://ssd-api.jpl.nasa.gov/fireball.api?date-min=${minDate}`,
+    headers: { }
+  }
+  axios(config)
+  // If request works
+  .then(response => {
+    console.log(JSON.stringify(response.data.data[1]));
+    res.render("index", {
+      
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
+  
+});
+
+app.get("/", (req, res) => {
+
+});
 
 // Start express/nodemon server
 app.listen(5000);
