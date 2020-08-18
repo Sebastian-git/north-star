705dd1af2d803ff8d6f3967fb11ce6b195e9b484
Sebastian-git <sebastian01cevallos@gmail.com>
2020-08-18 11:59:08 -0700

    Successfully saved to firebase

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -17,7 +17,7 @@ router.post("/searchResults", (req, res) => {
   // If request works
   .then(response => {
     // Add date, energy, velocity, longitude and latitude into array as a string
-    let currentData = [];
+    let currentData = []; 
     for (let i = 0; i < response.data.count; i++) {
       currentData.push([response.data.data[i][0], response.data.data[i][1], response.data.data[i][8], response.data.data[i][5], response.data.data[i][3], " "]);
     }
@@ -33,6 +33,7 @@ router.post("/searchResults", (req, res) => {
 
 router.post("/favorite", (req, res) => {
   console.log(req.body);
+  firebase.doSaveFireball(req.body.favorites);
 })
 
 module.exports = router;
